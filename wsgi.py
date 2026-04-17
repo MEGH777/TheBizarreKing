5efe0cc35c6343cfbc7a65538217735cee3e61fa
MEGH777 <83692043+MEGH777@users.noreply.github.com>
2026-04-17 21:54:30 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from app import app, socketio
 import os
 
 port = int(os.environ.get('PORT', 5000))
-socketio.run(app, host='0.0.0.0', port=port)
+socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
